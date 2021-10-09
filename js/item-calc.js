46a26a965a7296e4bd76e49527093f9002c63fb9
dev2l0per <z-_-z1-@naver.com>
2021-10-09 23:08:56 +0900

    [Feat] Item Cost Calculate

---
+++ js/item-calc.js
@@ -0,0 +1,15 @@
+function calculateCost() {
+	const from = document.querySelector('#from');
+	const to = document.querySelector('#to');
+	const fromValue = from.options[from.selectedIndex].value;
+	const toValue = to.options[to.selectedIndex].value;
+
+	fetch(`https://pkm.gg/api/item-upgrade-cost/?start_level=${fromValue}&end_level=${toValue}`, {
+		"method": 'GET',
+	}).then((response) => (
+		response.json()
+	)).then((data) => {
+		const costP = document.querySelector('.cost-calc > #cost');
+		costP.textContent = data['cost'];
+	});
+}
